7bec2e692e8adad175fbcc7fe3efd91b9d664877
SheronSuditha <github@sudithakumarasinghe.me>
2019-09-04 02:58:07 +1000

    API : server startup event.
    Added log on "listening" event.

--- API/server.js
+++ API/server.js
@@ -4,4 +4,8 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
 
+server.on('listening', () => {
+    console.log("Running server on port 3000")
+})
+
 
